f55ccb861332704d1b51715f536a88b88f586406
EduardoPach <eduardo.pach@hotmail.com>
2023-06-19 09:08:11 -0300

    Moved functions to their own module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::prelude::*;
+use treerustler::utils;
+
+fn create_vector(size: usize, n_classes: u8) -> Vec<u8> {
+    let mut rng: ThreadRng = rand::thread_rng();
+    (0..size).map(|_| rng.gen_range(0..n_classes)).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<u8> = create_vector(10, 2);
+    println!("{:?}", v);
+    let entropy: f64 = utils::entropy_loss(&v);
+    println!("Entropy Loss: {}", entropy);
+    let gini: f64 = utils::gini_index(&v);
+    println!("Gini Index: {}", gini);
 }
