88f94449988544c49324053a22033c7a92a9a4b2
EduardoPach <eduardo.pach@hotmail.com>
2023-06-21 12:45:33 -0300

    Added lt_eq method to Data

--- src/lib.rs
+++ src/lib.rs
@@ -180,7 +180,7 @@ pub mod data {
             _temp.into_iter().map(|x| f64::from_bits(x)).collect()
         }
 
-        pub fn gt_eq(&self, col_idx: usize, threshold: f64) -> Vec<bool> {
+        pub fn lt_eq(&self, col_idx: usize, threshold: f64) -> Vec<bool> {
             let col_values: Vec<f64> = self.get_col(col_idx);
             col_values.iter().map(|&x| x <= threshold).collect()
         }
